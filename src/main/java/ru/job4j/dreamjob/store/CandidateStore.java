e88e8fddc7ce0ca5158021550beb6a2210e99f77
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-09-06 13:27:22 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,27 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1, "John", "Junior Java Developer"));
+        candidates.put(2, new Candidate(2, "Mike", "Middle Java Developer"));
+        candidates.put(3, new Candidate(3, "Selena", "Senior Java Developer"));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
