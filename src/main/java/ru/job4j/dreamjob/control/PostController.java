0d74069f76931aa52bbcd6c011f3d20dfbdc55cf
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-09-17 13:26:32 +0300

    4. PostController.updatePost. Редактирование вакансии. [#504850]

--- src/main/java/ru/job4j/dreamjob/control/PostController.java
+++ src/main/java/ru/job4j/dreamjob/control/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
@@ -32,5 +33,17 @@ public class PostController {
         System.out.println(post.getName());
         System.out.println(post.getDescription());
         return "redirect:/posts";
+    }
+
+    @PostMapping("/updatePost")
+    public String updatePost(@ModelAttribute Post post) {
+        store.update(post);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/formUpdatePost/{postId}")
+    public String formUpdatePost(Model model, @PathVariable("postId") int id) {
+        model.addAttribute("post", store.findById(id));
+        return "updatePost";
     }
 }
--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -30,4 +30,19 @@ public class PostStore {
         post.setId(counter.getAndIncrement());
         posts.put(post.getId(), post);
     }
+
+    public Post findById(int id) {
+        Post post = posts.get(id);
+        int postId = post.getId();
+        String postName = post.getName();
+        String postDesc = post.getDescription();
+        return new Post(postId, postName, postDesc);
+    }
+
+    public void update(Post post) {
+        System.out.println(post.getId());
+        System.out.println(post.getName());
+        System.out.println(post.getDescription());
+        posts.put(post.getId(), post);
+    }
 }
