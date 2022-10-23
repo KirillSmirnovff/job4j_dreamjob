2d976eee28657e281d92b0fcdd4b9e3998ccb44b
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-10-23 14:06:15 +0600

    2. Связь слоев через Spring DI. [#504856 #354071]

--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.store;
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
 import java.util.Collection;
@@ -7,18 +8,14 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class CandidateStore {
-    private static final CandidateStore INST = new CandidateStore();
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private final AtomicInteger counter = new AtomicInteger();
 
     private CandidateStore() {
-    }
-
-    public static CandidateStore instOf() {
-        return INST;
     }
 
     public Collection<Candidate> findAll() {
@@ -35,9 +32,6 @@ public class CandidateStore {
     }
 
     public void replace(Candidate candidate) {
-        System.out.println(candidate.getId());
-        System.out.println(candidate.getName());
-        System.out.println(candidate.getDescription());
         candidates.replace(candidate.getId(), candidate);
     }
 }
