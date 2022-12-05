0f2f8f1e44f6d482fed66041480bd4cc060b457b
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-12-05 18:07:06 +0600

    1. Страница login.html [#504863]

--- src/main/java/ru/job4j/dreamjob/service/UserService.java
+++ src/main/java/ru/job4j/dreamjob/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.store.UserDBStore;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @ThreadSafe
@@ -25,6 +26,17 @@ public class UserService {
         Optional<User> result = Optional.empty();
         if (isValid(user)) {
             result = store.add(user);
+        }
+        return result;
+    }
+
+    public Optional<User> findUserByEmailAndPassword(String email, String password) {
+        Optional<User> result = store.findUserByEmailAndPassword(email);
+        if (result.isPresent()) {
+            String userPassword = result.get().getPassword();
+            if (!Objects.equals(password, userPassword)) {
+                result = Optional.empty();
+            }
         }
         return result;
     }
--- src/main/java/ru/job4j/dreamjob/store/UserDBStore.java
+++ src/main/java/ru/job4j/dreamjob/store/UserDBStore.java
@@ -9,6 +9,7 @@ import ru.job4j.dreamjob.model.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 @Repository
@@ -17,9 +18,19 @@ public class UserDBStore {
     private final BasicDataSource pool;
     private static final Logger LOG = LoggerFactory.getLogger(PostDBStore.class.getName());
     private static final String INSERT_USER = "INSERT INTO users(name, email, password) VALUES (?, ?, ?)";
+    private static final String SELECT_USER = "SELECT * FROM users WHERE email = ?";
 
     public UserDBStore(BasicDataSource pool) {
         this.pool = pool;
+    }
+
+    private User getUser(ResultSet resultSet) throws SQLException {
+        return new User(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("email"),
+                resultSet.getString("password")
+        );
     }
 
     public Optional<User> add(User user) {
@@ -35,6 +46,22 @@ public class UserDBStore {
                 if (rs.next()) {
                     user.setId(rs.getInt(1));
                     result = Optional.of(user);
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Error!", e);
+        }
+        return result;
+    }
+
+    public Optional<User> findUserByEmailAndPassword(String email) {
+        Optional<User> result = Optional.empty();
+        try (Connection connection = pool.getConnection();
+            PreparedStatement statement =  connection.prepareStatement(SELECT_USER)) {
+            statement.setString(1, email);
+            try (ResultSet rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    result = Optional.of(getUser(rs));
                 }
             }
         } catch (Exception e) {
