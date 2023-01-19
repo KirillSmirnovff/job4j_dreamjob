65b1ca838f56bcd56b150aa505c0fd6b1a19c928
KirillSmirnovff <smirnovk.1990@gmail.com>
2023-01-19 15:19:06 +0300

    3. Filter [#504865 #372063]

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -1,14 +1,31 @@
 package ru.job4j.dreamjob.filter;
 
 import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class AuthFilter implements Filter {
 
+    private final Set<String> pages = new HashSet<>(List.of(
+            "loginPage",
+            "login",
+            "formAddUser",
+            "success",
+            "fail",
+            "registration")
+    );
+
+
+    private boolean urlContains(String url) {
+        return pages.stream().anyMatch(url::endsWith);
+    }
     @Override
     public void doFilter(
             ServletRequest request,
@@ -17,8 +34,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login") || uri.endsWith("formAddUser")
-                || uri.endsWith("success") || uri.endsWith("fail") || uri.endsWith("registration")) {
+        if (urlContains(uri)) {
             chain.doFilter(req, res);
             return;
         }
